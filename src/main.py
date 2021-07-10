1ad923ff52f5db62f548b52435b514e848f14866
Kecy Jones <Miskecysss@gmail.com>
2021-07-10 15:29:06 -0300

    Multiple detections logic

--- src/main.py
+++ src/main.py
@@ -1,33 +1,58 @@
 import cv2 as cv
+import numpy as np
+# import os
 
-haystack_img = cv.imread('src/sample01/idleon_farm.jpg', cv.IMREAD_UNCHANGED)
-needle_img = cv.imread('src/sample01/idleon_cooler.jpg', cv.IMREAD_UNCHANGED)
+# os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# import sys
+# np.set_printoptions(threshold=sys.maxsize)
+
+haystack_img = cv.imread('src/idleon_farm.jpg', cv.IMREAD_UNCHANGED)
+needle_img = cv.imread('src/idleon_cooler.jpg', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+print(result)
 
-# get the best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+threshold = 0.40
+locations = np.where(result >= threshold)
+# the np.where() return value will look like this:
+# (array([226, 286], dtype=int64), array([ 98, 422], dtype=int64))
+print(locations)
 
-print('Best match top left position: %s' % str(max_loc))
-print('Best match confidence: %s' % max_val)
+# we can zip those up into position tuples
+locations = list(zip(*locations[::-1]))
+# zip explanation:
+# res = [[10, 20, 30], [7, 8, 9]]
+# res[::-1]
+# [[7, 8, 9], [10, 20, 30]]
+# zip(*res[::-1])
+# <zip object at 0x012DEB28>
+# list(zip(*res[::-1]))
+# [(7, 10), (8, 20), (9, 30)]
+print(locations)
 
-threshold = 0.5
-if max_val >= threshold:
+if locations:
     print('Found needle.')
 
-    # get dimension of the needle image
     needle_w = needle_img.shape[1]
     needle_h = needle_img.shape[0]
+    line_color = (0, 255, 0)
+    line_thickness = 2
+    line_type = cv.LINE_4
 
-    top_left = max_loc
-    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+    # need to loop over all the locations and draw their rectangle
+    for loc in locations:
+        # determine the box positions
+        top_left = loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
 
-    cv.rectangle(haystack_img, top_left, bottom_right, color=(
-        0, 255, 0), thickness=2, lineType=cv.LINE_4)
+        # draw the box
+        cv.rectangle(haystack_img, top_left, bottom_right,
+                     line_color, line_thickness, line_type)
 
-    cv.imwrite('src/sample01/result.jpg', haystack_img)
-
-    cv.imshow('Result', haystack_img)
+    # cv.imwrite('result.jpg', haystack_img)
+    cv.imshow('Matches', haystack_img)
     cv.waitKey()
+
 else:
-    print('Needle not found.')
+    print('Needles not found.')
