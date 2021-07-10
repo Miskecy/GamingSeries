567831f434f17fc3110e37618939f5aba0ccd4d8
Kecy Jones <Miskecysss@gmail.com>
2021-07-10 19:02:40 -0300

    Improve real time detection

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 import cv2 as cv
 from time import time
 from windowcapture import WindowCapture
+from vision import Vision
 
+# initialize the WindowCapture
 wincap = WindowCapture('Legends Of Idleon')
+# initialize the Vision class
+vision_cooler = Vision('src/idleon_melty_cube.jpg')
 
 loop_time = time()
 while(True):
 
     screenshot = wincap.get_screenshot()
 
-    cv.imshow('Computer Vision', screenshot)
+    # cv.imshow('Computer Vision', screenshot)
+
+    # display the processed image
+    points = vision_cooler.find(screenshot, 0.5, 'rectangles')
 
     print('FPS {}'.format(1 / (time() - loop_time)))
     loop_time = time()
