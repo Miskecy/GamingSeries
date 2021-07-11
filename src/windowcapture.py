70a5929df27b60715ab0a79eb5f251d83c914d99
Kecy Jones <Miskecysss@gmail.com>
2021-07-11 00:23:45 -0300

    Fix alpha img issue

--- src/windowcapture.py
+++ src/windowcapture.py
@@ -16,11 +16,15 @@ class WindowCapture:
     offset_y = 0
 
     # constructor
-    def __init__(self, window_name):
-        # find the handle for the window we want to capture
-        self.hwnd = win32gui.FindWindow(None, window_name)
-        if not self.hwnd:
-            raise Exception('Window not found: {}'.format(window_name))
+    def __init__(self, window_name=None):
+        # find the handle for the window we want to capture.
+        # if no window name is given, capture the entire screen
+        if window_name is None:
+            self.hwnd = win32gui.GetDesktopWindow()
+        else:
+            self.hwnd = win32gui.FindWindow(None, window_name)
+            if not self.hwnd:
+                raise Exception('Window not found: {}'.format(window_name))
 
         # get the window size
         window_rect = win32gui.GetWindowRect(self.hwnd)
@@ -51,13 +55,13 @@ class WindowCapture:
         cDC.BitBlt((0, 0), (self.w, self.h), dcObj,
                    (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
 
-        # save the screenshot
-        # dataBitMap.SaveBitmapFile(cDC, 'src/debug.bmp')
+        # convert the raw data into a format opencv can read
+        # dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
         signedIntsArray = dataBitMap.GetBitmapBits(True)
         img = np.fromstring(signedIntsArray, dtype='uint8')
         img.shape = (self.h, self.w, 4)
 
-        # Free Resources
+        # free resources
         dcObj.DeleteDC()
         cDC.DeleteDC()
         win32gui.ReleaseDC(self.hwnd, wDC)
@@ -66,16 +70,14 @@ class WindowCapture:
         # drop the alpha channel, or cv.matchTemplate() will throw an error like:
         #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
         #   && _img.dims() <= 2 in function 'cv::matchTemplate'
-
-        # img = img[..., :3]
+        img = img[..., :3]
 
         # make image C_CONTIGUOUS to avoid errors that look like:
         #   File ... in draw_rectangles
         #   TypeError: an integer is required (got type tuple)
         # see the discussion here:
         # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
-
-        # img = np.ascontiguousarray(img)
+        img = np.ascontiguousarray(img)
 
         return img
 
