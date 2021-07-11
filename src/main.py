eb217310e0a13de69cb4ed187c0b31eccd8ed5ca
Kecy Jones <Miskecysss@gmail.com>
2021-07-11 00:23:22 -0300

    Apply HSV filter

--- src/main.py
+++ src/main.py
@@ -1,23 +1,42 @@
 import cv2 as cv
+import numpy as np
 from time import time
 from windowcapture import WindowCapture
 from vision import Vision
+from hsvfilter import HsvFilter
 
 # initialize the WindowCapture
 wincap = WindowCapture('Legends Of Idleon')
 # initialize the Vision class
-vision_cooler = Vision('src/idleon_melty_cube.jpg')
+vision_melty = Vision('img/idleon_melty_cube_hsv_filter.jpg')
+# initialize the trackbar window
+vision_melty.init_control_gui()
+
+# melty cube HSV filter
+hsv_filter = HsvFilter(70, 152, 114, 104, 255, 255, 255, 91, 34, 63)
 
 loop_time = time()
 while(True):
 
+    # get an updated image of the game
     screenshot = wincap.get_screenshot()
 
-    # cv.imshow('Computer Vision', screenshot)
+    if screenshot is None:
+        continue
+
+    # pre-process the image
+    processed_image = vision_melty.apply_hsv_filter(screenshot, hsv_filter)
+
+    # do object detection
+    rectangles = vision_melty.find(processed_image, 0.5)
+
+    # draw the detection results into the original image
+    output_image = vision_melty.draw_rectangles(processed_image, rectangles)
 
     # display the processed image
-    points = vision_cooler.find(screenshot, 0.5, 'rectangles')
+    cv.imshow('Matches', output_image)
 
+    # debug the loop rate
     print('FPS {}'.format(1 / (time() - loop_time)))
     loop_time = time()
 
