183e888d54dd6500595310f01b8c655f7e3ff1a1
Kecy Jones <Miskecysss@gmail.com>
2021-07-11 13:04:17 -0300

    Add ORB feature

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from edgefilter import EdgeFilter
 # initialize the WindowCapture
 wincap = WindowCapture('Legends Of Idleon')
 # initialize the Vision class
-vision_melty = Vision('img/idleon_melty_cube_hsv_filter.jpg')
+vision_melty = Vision('img/idleon_melty_cube.jpg')
 # initialize the trackbar window
 vision_melty.init_control_gui()
 
@@ -26,10 +26,10 @@ while(True):
         continue
 
     # pre-process the image
-    processed_image = vision_melty.apply_hsv_filter(screenshot)
+    # processed_image = vision_melty.apply_hsv_filter(screenshot)
 
     # do edge detection
-    edges_image = vision_melty.apply_edge_filter(processed_image)
+    # edges_image = vision_melty.apply_edge_filter(processed_image)
 
     # do object detection
     # rectangles = vision_melty.find(processed_image, 0.5)
@@ -37,8 +37,31 @@ while(True):
     # draw the detection results into the original image
     # output_image = vision_melty.draw_rectangles(processed_image, rectangles)
 
+    # keypoints searching
+    keypoint_image = screenshot
+    kp1, kp2, matches, match_points = vision_melty.match_keypoints(
+        keypoint_image)
+    match_image = cv.drawMatches(
+        vision_melty.needle_img,
+        kp1,
+        keypoint_image,
+        kp2,
+        matches,
+        None
+    )
+
+    if match_points:
+        # find the center of all the matched features
+        center_point = vision_melty.centeroid(match_points)
+
+        # account for the width of the needle image that appears on the left
+        center_point[0] += vision_melty.needle_w
+
+        # drawn the found center point on the output image
+        match_image = vision_melty.draw_crosshairs(match_image, [center_point])
+
     # display the processed image
-    cv.imshow('Matches', edges_image)
+    cv.imshow('Matches', match_image)
 
     # debug the loop rate
     print('FPS {}'.format(1 / (time() - loop_time)))
